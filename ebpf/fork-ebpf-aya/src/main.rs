eb4a1af2a436772fc13d5fa1d1387244655df359
Michal Rostecki <vadorovsky@gmail.com>
2023-02-05 09:01:36 +0100

    ebpf/aya: Change the error type in try function
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- ebpf/fork-ebpf-aya/src/main.rs
+++ ebpf/fork-ebpf-aya/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{helpers::bpf_printk, macros::tracepoint, programs::TracePointContext};
+use aya_bpf::{cty::c_long, helpers::bpf_printk, macros::tracepoint, programs::TracePointContext};
 use aya_btf_map::{macros::btf_map, HashMap};
 
 #[btf_map]
@@ -11,23 +11,19 @@ static mut PID_MAP: HashMap<i32, i32, 1024> = HashMap::new();
 pub fn fork(ctx: TracePointContext) -> u32 {
     match try_fork(ctx) {
         Ok(ret) => ret,
-        Err(ret) => ret,
+        Err(ret) => ret as u32,
     }
 }
 
-fn try_fork(ctx: TracePointContext) -> Result<u32, u32> {
+fn try_fork(ctx: TracePointContext) -> Result<u32, c_long> {
     // Load the pointer to the filename. The offset value can be found running:
     // sudo cat /sys/kernel/debug/tracing/events/sched/sched_process_fork/format
     const PARENT_PID_OFFSET: usize = 24;
     const CHILD_PID_OFFSET: usize = 44;
-    let parent_pid: i32 = unsafe { ctx.read_at(PARENT_PID_OFFSET).map_err(|e| e as u32)? };
-    let child_pid: i32 = unsafe { ctx.read_at(CHILD_PID_OFFSET).map_err(|e| e as u32)? };
+    let parent_pid: i32 = unsafe { ctx.read_at(PARENT_PID_OFFSET)? };
+    let child_pid: i32 = unsafe { ctx.read_at(CHILD_PID_OFFSET)? };
 
-    unsafe {
-        PID_MAP
-            .insert(&parent_pid, &child_pid, 0)
-            .map_err(|e| e as u32)?
-    };
+    unsafe { PID_MAP.insert(&parent_pid, &child_pid, 0)? };
 
     unsafe {
         bpf_printk!(
