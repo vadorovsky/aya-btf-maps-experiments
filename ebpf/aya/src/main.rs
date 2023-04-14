9c5ec9a19052a7838b24a2639d18d8a4c31aa9be
Michal Rostecki <vadorovsky@protonmail.com>
2023-04-14 18:41:26 +0200

    ebpf: Add struct and functions with generics
    To ensure that we sanitize their names properly and generate valid BTF.

--- ebpf/aya/src/main.rs
+++ ebpf/aya/src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{cty::c_long, macros::tracepoint, programs::TracePointContext};
+use aya_bpf::{cty::c_long, macros::tracepoint, programs::TracePointContext, BpfContext};
 use aya_btf_map::{macros::btf_map, HashMap};
+use aya_log_ebpf::{error, info, WriteToBuf};
 
 #[btf_map]
 static mut PID_MAP: HashMap<i32, i32, 1024> = HashMap::new();
+
+#[btf_map]
+static mut DUMMY_MAP: HashMap<i32, Dummy<u32, 1024>, 1024> = HashMap::new();
 
 #[tracepoint(name = "fork")]
 pub fn fork(ctx: TracePointContext) -> u32 {
@@ -20,12 +24,49 @@ fn try_fork(ctx: TracePointContext) -> Result<u32, c_long> {
     // sudo cat /sys/kernel/debug/tracing/events/sched/sched_process_fork/format
     const PARENT_PID_OFFSET: usize = 24;
     const CHILD_PID_OFFSET: usize = 44;
-    let parent_pid: i32 = unsafe { ctx.read_at(PARENT_PID_OFFSET)? };
-    let child_pid: i32 = unsafe { ctx.read_at(CHILD_PID_OFFSET)? };
+    let parent_pid: i32 = unsafe {
+        ctx.read_at(PARENT_PID_OFFSET).map_err(|e| {
+            log_err(&ctx, e);
+            e
+        })?
+    };
+    let child_pid: i32 = unsafe {
+        ctx.read_at(CHILD_PID_OFFSET).map_err(|e| {
+            log_err2(&ctx, e);
+            e
+        })?
+    };
+
+    let dummy = Dummy {
+        a: 0u32,
+        b: [0; 1024],
+    };
+    info!(&ctx, "dummy: {}", dummy.a);
+
+    if let Some(dummy) = unsafe { DUMMY_MAP.get(&parent_pid) } {
+        info!(&ctx, "dummy: {}", dummy.a);
+    }
 
     unsafe { PID_MAP.insert(&parent_pid, &child_pid, 0)? };
 
     Ok(0)
+}
+
+// Just a dummy function which has a generic.
+pub fn log_err<C: BpfContext>(ctx: &C, err: c_long) {
+    error!(ctx, "error code: {}", err);
+}
+
+// Same, but with 2 generics.
+pub fn log_err2<C: BpfContext, E: WriteToBuf>(ctx: &C, err: E) {
+    error!(ctx, "error: {}", err);
+}
+
+// Keep tryin' with generics.
+#[repr(C)]
+pub struct Dummy<T, const N: usize> {
+    pub a: T,
+    pub b: [u8; N],
 }
 
 #[panic_handler]
